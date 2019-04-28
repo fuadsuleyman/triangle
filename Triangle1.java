ce076edac8457a1d67f8b97903541e20107b672c
Fuad Suleymanov <fuadsuleymanov@outlook.com>
2019-04-28 14:50:39 +0400

    Add file

---
+++ Triangle1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Triangle1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt(); // burda n triangle-in setr sayidi
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
